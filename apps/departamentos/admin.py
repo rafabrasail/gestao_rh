9b7879dbf19eb2fba8c31f1baefc62aeefbc8376
rafabrasail <rafaelrosenberg89@gmail.com>
2019-05-12 10:18:52 -0300

    aplicação deperdamento

---
+++ apps/departamentos/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Departamentos
+
+admin.site.register(Departamentos)
+
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -30,15 +30,17 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = {
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.empresas',
-}
+    'apps.empresa',
+    'apps.funcionarios',
+    'apps.departamentos',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'apps.empresas',
 ]
 
 ROOT_URLCONF = 'gestao_rh.urls'
